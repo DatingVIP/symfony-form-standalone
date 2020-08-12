dc1b6810590cb0ca2606a246a6e4dcbb9ee67916
Artur Plawecki <aplawecki@firstbeatmedia.com>
2020-08-12 12:59:42 +0200

    custom html form type

---
+++ Type/HtmlType.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Form\Standalone\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class HtmlType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefaults(
+            [
+                'class' => ''
+            ]
+        );
+
+        $resolver->setRequired(['html']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        parent::buildView($view, $form, $options);
+
+        $view->vars['html']  = ($options['html'] ?? null);
+        $view->vars['class'] = ($options['class'] ?? '');
+    }
+}
